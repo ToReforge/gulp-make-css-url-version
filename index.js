c68d1ecbda1bdd1961420b6500032cfc7ad029fd
Pawel Nowak <pawel@pawelnowak.com>
2017-04-06 14:19:51 +0200

    Excluding https://

--- index.js
+++ index.js
@@ -72,7 +72,7 @@ module.exports = function (options) {
             url = url.replace(/\?[\s\S]*$/, "").trim();
             url = url.replace(/['"]*/g, "");
 
-            if (url.indexOf("base64,") > -1 || url.indexOf("about:blank") > -1 || url.indexOf("http://") > -1 || url === '/') {
+            if (url.indexOf("base64,") > -1 || url.indexOf("about:blank") > -1 || url.indexOf("http://") > -1 || url.indexOf("https://") > -1 || url === '/') {
                 return str;
             }
 
